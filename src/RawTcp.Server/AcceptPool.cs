eb6cecef516a3540827a97dbb6b30ecd3a0e7aac
Karl O'Brien <karlobrien@gmail.com>
2020-04-05 10:59:09 +0100

    Pooling
    some pools

---
+++ src/RawTcp.Server/AcceptPool.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RawTcp.Server
+{
+    public class AcceptPool
+    {
+        //a pool of socket event args
+    }
+
+    public class SendRecPool
+    {
+        //this is where the pipe gets created
+        //holds all socket event args
+    }
+}
---
+++ src/RawTcp.Server/SocketReceiver.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace RawTcp.Server
+{
+    public class SocketReceiver
+    {
+        private readonly Socket _socket;
+        private readonly SocketAsyncEventArgs _eventArgs = new SocketAsyncEventArgs();
+
+        public SocketReceiver(Socket socket)
+        {
+            _socket = socket;
+
+            _eventArgs.UserToken = "Karl";
+            _eventArgs.Completed += (_, e) => { Console.WriteLine($""); };
+        }
+
+
+        public Task ReceiveAsync(Memory<byte> buffer)
+        {
+#if NETCOREAPP
+            _eventArgs.SetBuffer(buffer);
+#else
+            var segment = buffer.GetArray();
+
+            _eventArgs.SetBuffer(segment.Array, segment.Offset, segment.Count);
+#endif
+            if (!_socket.ReceiveAsync(_eventArgs))
+            {
+                //_awaitable.Complete(_eventArgs.BytesTransferred, _eventArgs.SocketError);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
